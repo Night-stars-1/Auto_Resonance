fd3b125b1cbe063f3fb0eea5472f2c73b3886af6
Night-stars-1 <nujj1042633805@gmail.com>
2024-05-05 20:37:44 +0800

    chore: 更新测试用例

--- tests/tests/test_goods.py
+++ tests/tests/test_goods.py
@@ -1,7 +1,7 @@
 """
 Author: Night-stars-1 nujj1042633805@gmail.com
 Date: 2024-04-10 22:45:55
-LastEditTime: 2024-04-28 19:58:17
+LastEditTime: 2024-05-05 20:37:30
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
 """
 
@@ -24,112 +24,14 @@ from core.goods import SHOP, show
 
 
 def test_get_goods_info_srap():
-    route = get_goods_info_srap(
-        city_book={
-            "totalMaxBook": 4,
-            "profitThreshold": 16000,
-            "priceThreshold": 500,
-        },
-        skill_level={
-            "伊尔": 0,
-            "卡洛琳": 0,
-            "叶珏": 0,
-            "塞西尔": 0,
-            "多萝西": 0,
-            "奈弥": 0,
-            "山岚": 0,
-            "星花": 0,
-            "朱利安": 2,
-            "狮鬃": 0,
-            "瑞秋": 0,
-            "瓦伦汀": 0,
-            "艾略特": 0,
-            "菲妮娅": 0,
-            "闻笙": 0,
-            "阿知波": 0,
-            "隼": 0,
-            "雷火": 0,
-            "魇": 0,
-            "黛丝莉": 0,
-        },
-        station_level={
-            "7号自由港": 19,
-            "修格里城": 13,
-            "曼德矿场": 12,
-            "澄明数据中心": 18,
-        },
-        negotiate_price={
-            "buyTired": 12,
-            "sellTired": 12,
-            "7号自由港": 2,
-            "修格里城": 2,
-            "曼德矿场": 2,
-            "淘金乐园": 2,
-            "澄明数据中心": 2,
-            "荒原站": 2,
-            "铁盟哨站": 2,
-            "阿妮塔战备工厂": 2,
-            "阿妮塔能源研究所": 2,
-        },
-        max_goods_num=625,
-    )
+    route = get_goods_info_srap()
     LOGGER.info(route)
     print(show(route))
     assert route.city_data[0].profit != 0 and route.city_data[1].profit != 0
 
 
 def test_get_goods_info_kmou():
-    route = get_goods_info_kmou(
-        city_book={
-            "totalMaxBook": 4,
-            "profitThreshold": 7000,
-            "priceThreshold": 500,
-        },
-        skill_level={
-            "伊尔": 0,
-            "卡洛琳": 0,
-            "叶珏": 0,
-            "塞西尔": 0,
-            "多萝西": 0,
-            "奈弥": 0,
-            "山岚": 0,
-            "星花": 0,
-            "朱利安": 2,
-            "狮鬃": 0,
-            "瑞秋": 0,
-            "瓦伦汀": 0,
-            "艾略特": 0,
-            "菲妮娅": 0,
-            "闻笙": 0,
-            "阿知波": 0,
-            "隼": 0,
-            "雷火": 0,
-            "魇": 0,
-            "黛丝莉": 0,
-        },
-        station_level={
-            "7号自由港": 19,
-            "修格里城": 13,
-            "曼德矿场": 14,
-            "澄明数据中心": 17,
-            "阿妮塔发射中心": 0
-        },
-        negotiate_price={
-            "buyTired": 12,
-            "sellTired": 12,
-            "7号自由港": 2,
-            "修格里城": 2,
-            "曼德矿场": 2,
-            "淘金乐园": 2,
-            "澄明数据中心": 2,
-            "荒原站": 2,
-            "铁盟哨站": 2,
-            "阿妮塔战备工厂": 2,
-            "阿妮塔能源研究所": 2,
-        },
-        max_goods_num=625,
-        uuid=cfg.uuid.value,
-    )
+    route = get_goods_info_kmou()
     LOGGER.info(route)
     print(show(route))
     assert route.city_data[0].profit != 0 and route.city_data[1].profit != 0
@@ -142,19 +44,12 @@ def test_get_city_data_by_city_level():
         "曼德矿场": 12,
         "澄明数据中心": 18,
     }
-    result = SHOP(
-        goods_data=GoodsModel(goods=[]),
-        city_book={},
-        skill_level={},
-        station_level=station_level,
-        negotiate_price={},
-        max_goods_num=625,
-    ).get_city_data_by_city_level(station_level=station_level)
+    result = SHOP(goods_data=GoodsModel(goods=[])).get_city_data_by_city_level()
     print(result)
     assert result["7号自由港"].buy_num != 0.0
 
 
 if __name__ == "__main__":
     # test_get_goods_info_kmou()
-    test_get_goods_info_srap()
-    # test_get_city_data_by_city_level()
+    # test_get_goods_info_srap()
+    test_get_city_data_by_city_level()

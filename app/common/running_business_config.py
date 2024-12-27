031c26e6d10c35f7441fe80b26665dd62414deaa
Night-stars-1 <nujj1042633805@gmail.com>
2024-12-27 23:50:28 +0800

    feat: 抽象出SKILLS、CITYS、STATIONS方便配置

--- app/common/running_business_config.py
+++ app/common/running_business_config.py
@@ -1,12 +1,41 @@
 """
 Author: Night-stars-1 nujj1042633805@gmail.com
 Date: 2024-04-11 20:58:19
-LastEditTime: 2024-09-10 20:08:58
+LastEditTime: 2024-12-27 23:50:07
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
 """
 
 from qfluentwidgets import ConfigItem, QConfig
 
+from app.common.utils import read_json
+
+skill_data: dict = read_json("resources/goods/SkillData.json")
+
+SKILLS = list(skill_data.keys())
+
+# 七号自由港需要在下方特殊适配
+CITYS = [
+    "七号自由港",
+    "澄明数据中心",
+    "阿妮塔战备工厂",
+    "阿妮塔能源研究所",
+    "淘金乐园",
+    "曼德矿场",
+    "荒原站",
+    "铁盟哨站",
+    "修格里城",
+    "阿妮塔发射中心",
+    "海角城",
+]
+# 七号自由港需要在下方特殊适配
+STATIONS = [
+    "七号自由港",
+    "澄明数据中心",
+    "曼德矿场",
+    "修格里城",
+    "阿妮塔发射中心",
+    "海角城",
+]
 
 class RunningBusinessConfig(QConfig):
     """Config of application"""
@@ -15,59 +44,19 @@ class RunningBusinessConfig(QConfig):
     profitThreshold = ConfigItem("RunningBusiness", "profitThreshold", 100000, None)
     priceThreshold = ConfigItem("RunningBusiness", "priceThreshold", 500, None)
     maxGoodsNum = ConfigItem("RunningBusiness", "maxGoodsNum", 625, None)
-    tiredProfitThreshold = ConfigItem("RunningBusiness", "tiredProfitThreshold", 12000, None)
+    tiredProfitThreshold = ConfigItem(
+        "RunningBusiness", "tiredProfitThreshold", 12000, None
+    )
 
-    朱利安 = ConfigItem("SkillLevel", "朱利安", 0, None)
-    狮鬃 = ConfigItem("SkillLevel", "狮鬃", 0, None)
-    魇 = ConfigItem("SkillLevel", "魇", 0, None)
-    塞西尔 = ConfigItem("SkillLevel", "塞西尔", 0, None)
-    雷火 = ConfigItem("SkillLevel", "雷火", 0, None)
-    黛丝莉 = ConfigItem("SkillLevel", "黛丝莉", 0, None)
-    艾略特 = ConfigItem("SkillLevel", "艾略特", 0, None)
-    静流 = ConfigItem("SkillLevel", "静流", 0, None)
-    多萝西 = ConfigItem("SkillLevel", "多萝西", 0, None)
-    卡莲 = ConfigItem("SkillLevel", "卡莲", 0, None)
-    星花 = ConfigItem("SkillLevel", "星花", 0, None)
-    瑞秋 = ConfigItem("SkillLevel", "瑞秋", 0, None)
-    菲妮娅 = ConfigItem("SkillLevel", "菲妮娅", 0, None)
-    瓦伦汀 = ConfigItem("SkillLevel", "瓦伦汀", 0, None)
-    阿知波 = ConfigItem("SkillLevel", "阿知波", 0, None)
-    闻笙 = ConfigItem("SkillLevel", "闻笙", 0, None)
-    山岚 = ConfigItem("SkillLevel", "山岚", 0, None)
-    叶珏 = ConfigItem("SkillLevel", "叶珏", 0, None)
-    隼 = ConfigItem("SkillLevel", "隼", 0, None)
-    奈弥 = ConfigItem("SkillLevel", "奈弥", 0, None)
-    伊尔 = ConfigItem("SkillLevel", "伊尔", 0, None)
-    甘雅 = ConfigItem("SkillLevel", "甘雅", 0, None)
-    妮蔻拉 = ConfigItem("SkillLevel", "妮蔻拉", 0, None)
-    卡洛琳 = ConfigItem("SkillLevel", "卡洛琳", 0, None)
-    夏尔 = ConfigItem("SkillLevel", "夏尔", 0, None)
-    海因里希 = ConfigItem("SkillLevel", "海因里希", 0, None)
-    苔妮 = ConfigItem("SkillLevel", "苔妮", 0, None)
-    安菲娅 = ConfigItem("SkillLevel", "安菲娅", 0, None)
-    静流·逐夏 = ConfigItem("SkillLevel", "静流·逐夏", 0, None)
-    雅莱 = ConfigItem("SkillLevel", "雅莱", 0, None)
-    伊索斯 = ConfigItem("SkillLevel", "伊索斯", 0, None)
-    帕拉笛 = ConfigItem("SkillLevel", "帕拉笛", 0, None)
-    佩罗塔 = ConfigItem("SkillLevel", "佩罗塔", 0, None)
+    for skill in SKILLS:
+        locals()[skill] = ConfigItem("SkillLevel", skill, 0, None)
 
-    七号自由港砍抬次数 = ConfigItem("NegotiatePrice", "7号自由港", 0, None)
-    澄明数据中心砍抬次数 = ConfigItem("NegotiatePrice", "澄明数据中心", 0, None)
-    阿妮塔战备工厂砍抬次数 = ConfigItem("NegotiatePrice", "阿妮塔战备工厂", 0, None)
-    阿妮塔能源研究所砍抬次数 = ConfigItem("NegotiatePrice", "阿妮塔能源研究所", 0, None)
-    淘金乐园砍抬次数 = ConfigItem("NegotiatePrice", "淘金乐园", 0, None)
-    曼德矿场砍抬次数 = ConfigItem("NegotiatePrice", "曼德矿场", 0, None)
-    荒原站砍抬次数 = ConfigItem("NegotiatePrice", "荒原站", 0, None)
-    铁盟哨站砍抬次数 = ConfigItem("NegotiatePrice", "铁盟哨站", 0, None)
-    修格里城砍抬次数 = ConfigItem("NegotiatePrice", "修格里城", 0, None)
-    阿妮塔发射中心砍抬次数 = ConfigItem("NegotiatePrice", "阿妮塔发射中心", 0, None)
-    海角城砍抬次数 = ConfigItem("NegotiatePrice", "海角城", 0, None)
+    for city in CITYS:
+        # 特殊适配7号自由港
+        locals()[city] = ConfigItem("NegotiatePrice", city.replace("七号自由港", "7号自由港"), 0, None)
     buyTired = ConfigItem("NegotiatePrice", "buyTired", 8, None)
     sellTired = ConfigItem("NegotiatePrice", "sellTired", 8, None)
 
-    七号自由港声望 = ConfigItem("StationLevel", "7号自由港", 0, None)
-    澄明数据中心声望 = ConfigItem("StationLevel", "澄明数据中心", 0, None)
-    曼德矿场声望 = ConfigItem("StationLevel", "曼德矿场", 0, None)
-    修格里城声望 = ConfigItem("StationLevel", "修格里城", 0, None)
-    阿妮塔发射中心声望 = ConfigItem("StationLevel", "阿妮塔发射中心", 0, None)
-    海角城声望 = ConfigItem("StationLevel", "海角城", 0, None)
+    for station in STATIONS:
+        # 特殊适配7号自由港
+        locals()[station] = ConfigItem("StationLevel", station.replace("七号自由港", "7号自由港"), 0, None)

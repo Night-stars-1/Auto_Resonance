f32213bc75625676be9ba0c01dc78758445968a6
Night-stars-1 <nujj1042633805@gmail.com>
2025-08-02 19:13:59 +0800

    chore: 超时添加食用便当

--- auto/module/strength.py
+++ auto/module/strength.py
@@ -15,6 +15,24 @@ def check_shop_strength():
     total__strength = int(strength[1])
     return total__strength - cur_strength > 60
 
+def use_food():
+    click((1107, 606))
+    if not wait_gbr(
+        (61, 130),
+        BGR(26, 38, 91),
+        BGR(26, 38, 91)
+    ):
+        logger.error("未找到便当页面")
+        return False
+    click((1077, 430))
+    status = ocr_click("确认", cropped_pos1=(952, 485), cropped_pos2=(1022, 522))
+    click((1077, 430))
+    return status
+
+
+def use_candy():
+    click((655, 242))
+    return ocr_click("补充", cropped_pos1=(952, 565), cropped_pos2=(1023, 602))
 
 def use_strength():
     if not wait_gbr(
@@ -26,5 +44,8 @@ def use_strength():
     ):
         logger.error("未找到体力页面")
         return False
-    click((655, 242))
-    return ocr_click("补充", cropped_pos1=(952, 565), cropped_pos2=(1023, 602))
+    # image = screenshot()
+    # has_food = image.get_bgr((1052, 426))
+    # if has_food:
+    #     return use_food()
+    return use_candy()
